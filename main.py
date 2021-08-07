06a224f243a131c03308352d8e0577566e510b25
Harshit Shetty <harshitns23@gmail.com>
2021-08-07 10:14:13 +0530

    First commit

---
+++ main.py
@@ -0,0 +1,55 @@
+# UNIVERSAL CALCULATOR MAIN FILE
+"""
+from tkinter import *
+from arithmetic import *
+
+inpstring = ""
+
+
+def expression(event):
+    inpstring.append()
+
+
+window = Tk()
+window.geometry("500x500")
+window.title("Universal Calculator")
+mainentry = Entry(window)
+mainentry.place(x=10, y=10)
+
+
+btn0 = Button(window, text="0", width=20, bg="white", fg="black")
+btn0.place(x=200, y=480)
+
+btn0 = Button(window, text="1", width=20, bg="white", fg="black")
+btn0.place(x=280, y=480)
+
+btn0 = Button(window, text="2", width=20, bg="white", fg="black")
+btn0.place(x=200, y=460)
+
+btn0 = Button(window, text="3", width=20, bg="white", fg="black")
+btn0.place(x=280, y=460)
+
+btn0 = Button(window, text="4", width=20, bg="white", fg="black")
+btn0.place(x=200, y=440)
+
+btn0 = Button(window, text="5", width=20, bg="white", fg="black")
+btn0.place(x=280, y=440)
+
+btn0 = Button(window, text="6", width=20, bg="white", fg="black")
+btn0.place(x=200, y=420)
+
+btn0 = Button(window, text="7", width=20, bg="white", fg="black")
+btn0.place(x=280, y=420)
+
+btn0 = Button(window, text="8", width=20, bg="white", fg="black")
+btn0.place(x=200, y=400)
+
+btn0 = Button(window, text="9", width=20, bg="white", fg="black")
+btn0.place(x=280, y=400)
+
+# btn0 = Button(window, text="=", width=20, bg="white", fg="black")
+# btn0.place(x=200, y=480)
+
+window.mainloop()
+
+"""
